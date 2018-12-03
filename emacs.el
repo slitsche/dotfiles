9f0b7b2a1912207813cd54b6316d7cfb3f13264c
Stefan Litsche <slitsche@ZALANDO-29089.corp.ad.zalando.net>
2018-12-03 21:12:45 +0100

    Switch to org-static-blog as publisher

--- emacs.el
+++ emacs.el
@@ -466,4 +466,4 @@ If there is no plausible default, return nil."
  '(custom-enabled-themes (quote (dichromacy tsdh-light)))
  '(package-selected-packages
    (quote
-    (company-emacs-eclim eclim octave-mode evil-surround use-package elfeed org-edna htmlize auto-dim-other-buffers company yaml-mode slime org-bullets markdown-mode magit linum-relative helm-projectile go-eldoc go-autocomplete fill-column-indicator clj-refactor))))
+    (org-static-blog company-emacs-eclim eclim octave-mode evil-surround use-package elfeed org-edna htmlize auto-dim-other-buffers company yaml-mode slime org-bullets markdown-mode magit linum-relative helm-projectile go-eldoc go-autocomplete fill-column-indicator clj-refactor))))
--- sli-publish.el
+++ sli-publish.el
@@ -1,19 +1,34 @@
-(require 'ox-publish)
 
 ;; the variable org-html-metadata-timestamp-format
 ;; is not used for %d in org version 8.2.10
-(setq org-export-date-timestamp-format "%Y-%m-%d")
+;; (setq org-export-date-timestamp-format "%Y-%m-%d")
 
-;(setq  org-html-preamble-format '(("en" "<h1>hello sli</h1>")))
+
+;; <link rel=\"apple-touch-icon-precomposed\" href=\"static/favicon-152.png\">
+;; <link rel=\"msapplication-TitleImage\" href=\"static/favicon-144.png\">
+;; <link rel=\"msapplication-TitleColor\" href=\"#0141ff\">
+;; <script src=\"static/katex.min.js\"></script>
+;; <script src=\"static/auto-render.min.js\"></script>
+;; <link rel=\"stylesheet\" href=\"static/katex.min.css\">
+;; <script>document.addEventListener (\"DOMContentLoaded\", function() { renderMathInElement(document.body); });</script>
+
+(setq org-static-blog-page-header
+"<meta name=\"author\" content=\"Stefan Litsche\">
+<meta name=\"referrer\" content=\"no-referrer\">
+<link href= \"css/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link rel=\"icon\" href=\"img/favicon.ico\">
+<meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\">
+<meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">")
+
 (setq sli-biosoft-header
       (concat "<div id=\"header\">"
        "<h1>Stefan Litsche</h1>"
        "<ul>"
            "<li><a href=\"/\">Home</a></li>"
-           "<li><a href=\"/articles.html\">Blog</a></li>"
+;;           "<li><a href=\"/articles.html\">Blog</a></li>"
 ;;           "<li><a href=\"/projects/\">Projects</a></li>"
            "<li><a href=\"/impressum.html\">Impressum</a></li>"
-;;           "<li><a href=\"/resume.html\">Resume</a></li>"
+           "<li><a href=\"/rss.xml\">RSS</a></li>"
        "</ul>"
        "</div>"))
 
@@ -24,45 +39,15 @@
               "Handcrafted using Emacs and Orgmode"
               "</p>"))
 
-(setq org-publish-project-alist
-      `(
 
-        ;; ... add allthe components here (see below)...
-        ("org-notes"
-         :auto-sitemap t
-         ;; :sitemap-function my-blog-sitemap
-         :sitemap-sort-files chronologically
-         :sitemap-sort-folders nil
-         :sitemap-style list
-         :sitemap-title "Articles"
-         :sitemap-filename "articles.org"
-         :section-numbers nil
-         :base-directory "~/work/git/biosoft/src/"
-         :base-extension "org"
-         :publishing-directory "~/work/git/biosoft/html/"
-         :html-head-include-default-style nil
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/style.css\">"
-         :html-inline-images t
-         :html-doctype "html5"
-         :html-head-include-default-style nil
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4             ; Just the default for this project.
-         :auto-preamble nil
-         :html-preamble ,sli-biosoft-header
-         :html-postamble ,sli-biosoft-footer
-         ;:html-preamble-format  "<h1>hello sli</h1>" ;(("en" "<h1>hello sli</h1>"))
-         :with-toc nil
-         )
-
-        ("org-static"
-        :base-directory "~/work/git/biosoft/src/"
-        :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|html"
-        :publishing-directory "~/work/git/biosoft/html/"
-        :recursive t
-        :publishing-function org-publish-attachment
-        )
-
-        ("biosoft" :components ("org-notes" "org-static"))))
-
-(setq org-html-html5-fancy t)
+;;; org-static-blog
+(setq org-static-blog-publish-title "biosoft.de")
+(setq org-static-blog-publish-url "https://biosoft.de/")
+(setq org-static-blog-publish-directory "~/work/git/biosoft/html/")
+(setq org-static-blog-posts-directory "~/work/git/biosoft/src/")
+(setq org-static-blog-drafts-directory "~/work/git/biosoft/drafts/")
+(setq org-static-blog-enable-tags t)
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers nil)
+(setq org-static-blog-page-preamble sli-biosoft-header)
+(setq org-static-blog-page-postamble sli-biosoft-footer)
