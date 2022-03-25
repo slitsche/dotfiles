806a58d4c962a96eec09455109e4ceb3cbdecec8
Stefan Litsche <slitsche@biosoft.de>
2022-03-25 17:18:13 +0100

    Move orgmode config to a separate file

---
+++ orgconf.el
@@ -0,0 +1,239 @@
+
+;;; ============ ORG ============
+(require 'org)
+(require 'org-bullets)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "|" "DONE(d)")
+        (type  "MEET")))
+
+;; The following is important to be able to get my worklog from
+;; various files and nodes using agenda and log-mode
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+(setq org-catch-invisible-edits 'show-and-error)
+
+;; Setup folder structure
+(setq org-directory "~/Documents/org")
+(setq org-agenda-diary-file (concat org-directory "/diary.org"))
+(setq org-agenda-files (list "~/Documents/org/gtd.org"
+                             "~/Documents/org/todo.org"
+                             "~/Documents/org/privat.org"
+                             "~/Documents/org/networking.org"))
+(setq sli-notes-files '("~/Documents/org/notes.org"
+                        "~/Documents/org/emacs.org"
+                        "~/Documents/org/notes/cassandra-training.org"
+                        "~/Documents/org/projects.org"
+                        "~/Documents/org/notes/customer-inbox.org"
+                        "~/Documents/org/notes/segeln.org"))
+(defun sli-work-agenda ()
+  (interactive)
+  (org-agenda nil "w"))
+
+(setq sli-work-agenda
+      (seq-remove (lambda (x) (string-match "privat" x))
+                  org-agenda-files))
+
+(setq org-agenda-text-search-extra-files sli-notes-files)
+;http://sachachua.com/blog/2015/02/learn-take-notes-efficiently-org-mode/
+(setq org-refile-targets '((org-agenda-files . (:maxlevel . 3))
+                           (sli-notes-files . (:maxlevel . 3))
+                           (("~/Documents/org/someday.org") . (:maxlevel . 3))))
+
+;; https://dindi.garjola.net/zettelkustom.html
+(defun sli-zettel-template ()
+  "* %?\n:PROPERTIES:\n:DATE_CREATED: %U\n:FROM: %a\n:END:\n%i\n")
+
+(require 'org-id)
+;; if we store a link we want to generate an id-property
+(setq org-id-link-to-org-use-id t)
+
+(require 'org-ql)
+(require 'org-ql-view)
+;; (require 'helm-org-ql)
+
+;; inspired by https://dindi.garjola.net/zettelkustom.html
+(defun sli-zettel-backlinks ()
+  (interactive)
+  (let* ((id (org-entry-get (point) "ID"))
+         (query (cond (id `(link :target ,(concat "id:" id)))
+                      (t (error "Entry has no ID property"))))
+         (helm-query (concat "link:target=" (concat "\"" "id:" id "\"")))
+         (title (concat "Links to: " (org-get-heading t t)))
+         (org-agenda-tag-filter nil))
+    ;; (message helm-query)
+    ;; (helm-org-ql sli-notes-files :name helm-query)
+    (org-ql-search sli-notes-files query :title title)))
+
+;https://blog.aaronbieber.com/2016/01/30/dig-into-org-mode.html
+(setq org-capture-templates
+      '(("t" "Todo task inbox" entry
+         (file "inbox.org")
+         "* TODO %?\n%U")
+        ("T" "Todo task (active)" entry
+         (file "todo.org")
+         "* TODO %^{Title}\n%U\n%?")
+        ("r" "read" entry
+         (file "inbox.org")
+         "* %? :READ:\n%U\n")
+        ("n" "note" entry
+         (file "inbox.org")
+         "* %?\n%U")
+        ("o" "Orga" entry
+         (file+headline "gtd.org" "Orga Stuff")
+         "* DONE %^{Title}\nCLOSED: %U\n%?")
+        ("c" "Consulting" entry
+         (file+headline "gtd.org" "Consulting")
+         "* MEET %^{Title}\nCLOSED: %U\n%?"
+         :clock-in t)
+        ("m" "Meeting" entry
+         (file "todo.org")
+         "* MEET %^{Title}\nCLOSED: %U\n%?"
+         :clock-in t)
+        ("j" "Journal" entry
+         (file "todo.org")
+         "*** %?\nCLOSED: %U")
+        ("e" "Event log" entry
+         (file+datetree "notes/eventlog.org")
+         "*** %?\n%U")
+        ("h" "Health" entry
+         (file+datetree "no-agenda/health.org")
+         "*** %?\n%U")
+        ("s" "Segeln" entry
+         (file+headline "notes/segeln.org" "Zettels")
+         (function sli-zettel-template)
+         :empty-lines 1)))
+
+;; http://doc.norang.ca/org-mode.html#CustomAgendaViews
+;; https://emacs.stackexchange.com/questions/12517/how-do-i-make-the-timespan-shown-by-org-agenda-start-yesterday
+
+(setq org-agenda-custom-commands
+      (quote (("n" "Notes" tags "NOTE"
+               ((org-agenda-overriding-header "Notes")
+                (org-tags-match-list-sublevels t)))
+              ("r" "Review"
+               ((todo "TODO"
+                      ((org-agenda-files '("~/Documents/org/inbox.org"))
+                       (org-agenda-overriding-header "New Items")))
+                (tags-todo "-Boss/!TODO"
+                           ((org-agenda-overriding-header "Next Todos")
+                            (org-agenda-tags-todo-honor-ignore-options t)
+                            (org-agenda-todo-ignore-scheduled t)
+                            (org-agenda-todo-ignore-deadlines t)))
+                (tags-todo "+READ/!TODO"
+                           ((org-agenda-overriding-header "Next Read")
+                            (org-agenda-tags-todo-honor-ignore-options t)
+                            (org-agenda-todo-ignore-scheduled t)
+                            (org-agenda-todo-ignore-deadlines t)
+                            (org-agenda-files (append org-agenda-files sli-notes-files))))))
+              ("w" "My Work List"
+               ((agenda ""
+                        ((org-agenda-span 'day)
+                         (org-agenda-files sli-work-agenda)))
+                (tags-todo "-Boss-READ/!NEXT"
+                           ((org-agenda-tags-todo-honor-ignore-options t)
+                            (org-agenda-todo-ignore-scheduled t)
+                            (org-agenda-todo-ignore-deadlines t)
+                            (org-agenda-files (append sli-work-agenda sli-notes-files)))
+                           nil)
+                (tags-todo "READ/!NEXT"
+                           ((org-agenda-files (append org-agenda-files sli-notes-files))
+                            (org-agenda-overriding-header "Reading List")))))
+              ("d" "Delegated"
+               (;;;(agenda "" ((org-agenda-span 'week)))
+                (todo "WAIT"
+                           ((org-agenda-files sli-work-agenda)))))
+              ("p" "Private stuff"
+               ( ;;;(agenda "" ((org-agenda-span 'week)))
+                (tags-todo "-READ/TODO"
+                           ((org-agenda-files '("~/Documents/org/privat.org"))))))
+              ("U" tags-tree "+Boss"
+               ((org-show-context-detail 'minimal))))))
+
+(setq org-tag-alist '(("NOTE" . ?n)
+                      ("READ" . ?r)
+                      ("Qst" . ?f)
+                      ("ORGA" . ?o)
+                      ("CONS" . ?c)
+                      ("Boss" . ?b)
+                      ("Proj" . ?p)
+                      ("Geld" . ?m)
+                      ("PE" . ?e)
+                      ("Goal" . ?g)))
+
+(setq org-tags-exclude-from-inheritance '("NOTE" "Proj"))
+
+(setq org-stuck-projects
+      '("+Proj/-DONE" ("NEXT" "WAIT") nil nil))
+(add-to-list 'org-structure-template-alist
+             '("C" "#+BEGIN_COMMENT\n?\n#+END_COMMENT" "<!--?-->"))
+
+;;https://blog.aaronbieber.com/2017/03/19/organizing-notes-with-refile.html
+;; Make it possible to refile with helm
+(setq org-refile-use-outline-path 'file)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+
+;; I want to clean up my todo file which is a long list of TODOs and DONE in between
+;; https://stackoverflow.com/a/27043756/4096511
+;; the last parameter can be 'tree or 'agenda
+(defun sli-archive-done-tasks ()
+  (interactive)
+  (org-map-entries
+   (lambda ()
+     (org-archive-subtree)
+     (setq org-map-continue-from (org-element-property :begin (org-element-at-point))))
+   "/DONE" 'file))
+
+(defun sli-report-worklog ()
+  "Report on all consulting actions from worklog"
+  (interactive)
+  (message
+   (format "Consultings: %d"
+           (length
+            (org-map-entries t "CONS" 'tree)))))
+
+(defun sli-read-datetree-date (d)
+  "Parse a time string D and return a date to pass to the datetree functions."
+  (let ((dtmp (nthcdr 3 (parse-time-string d))))
+    (list (cadr dtmp) (car dtmp) (caddr dtmp))))
+
+(defun sli-refile-to-archive-datetree (&optional bfn)
+  "Refile an entry to a datetree under an archive."
+  (interactive)
+  (require 'org-datetree)
+  (let* ((bfn    (or bfn
+                     (find-file-noselect
+                      (expand-file-name "~/Documents/org/no-agenda/worklog.org"))))
+         (datetree-date (sli-read-datetree-date (org-read-date t nil)))
+         (target-pos  (with-current-buffer bfn
+                                (save-excursion
+                                  (org-datetree-find-date-create datetree-date)
+                                  (point)))))
+    (org-refile nil nil (list nil (buffer-file-name bfn) nil target-pos)))
+  (setq this-command 'my/org-refile-to-journal))
+
+;; Setup Org Babel
+(require 'ob-clojure)
+(setq org-babel-clojure-backend 'cider)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((sql . t)
+   ;;(sh . t)
+   (emacs-lisp . t)
+   (clojure . t)))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key (kbd "<f12>") 'sli-work-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+ ;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+(add-hook 'org-mode-hook 'org-indent-mode)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
